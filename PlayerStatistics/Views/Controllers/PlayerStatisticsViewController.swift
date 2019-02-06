ce8dbfcbe7aafdb8678fe9e6418064a91663e329
Ramanadeep <rdsinchd@gmail.com>
2019-02-06 17:37:54 +1100

    Added PlayerStatisticsViewController in the application.

---
+++ PlayerStatistics/Views/Controllers/PlayerStatisticsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlayerStatisticsViewController.swift
+//  PlayerStatistics
+//
+//  Created by raman singh on 06/02/19.
+//  Copyright © 2019 raman singh. All rights reserved.
+//
+
+import UIKit
+
+class PlayerStatisticsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
